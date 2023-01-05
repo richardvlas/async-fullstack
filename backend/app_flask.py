9a4cfe3202637d4deec0438e1357d3516ee0f68a
richard.vlas <vlasrichard@gmail.com>
2023-01-05 17:48:26 +0100

    added backend

---
+++ backend/app_flask.py
@@ -0,0 +1,38 @@
+import random
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+import time
+
+app = Flask(__name__)
+# Allow cross-origin requests from the frontend server 
+# (localhost:3000) to the backend server (localhost:5000)
+# This code enables CORS only for routes that match the 
+# /api/* pattern and only for requests from http://localhost:3000.
+# CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}})
+CORS(app, resources={
+    r"/api/short_request": {"origins": "http://localhost:3000"},
+    r"/api/long_request": {"origins": "http://localhost:3000"}
+})
+
+
+@app.route('/api/short_request', methods=['GET'])
+def short_request():
+    time.sleep(0.1)
+    return jsonify({'message': 'Short request: Response received!'})
+
+@app.route('/api/long_request', methods=['GET'])
+def long_request():
+    request_id = request.args.get('request_id')
+    time.sleep(random.randint(1, 4))
+    return jsonify({
+        'message': 'Long request: Response received!',
+        'request_id': request_id
+    })
+
+
+if __name__ == '__main__':
+    app.run(
+        host='localhost',
+        port=5000,
+        debug=True
+    )
